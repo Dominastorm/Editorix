34628b42482b72e7aeea7b7bf039e74687fa61a4
Dominastorm <dchawla228@gmail.com>
2022-04-23 14:14:17 +0530

    rgb channel visualisation

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 1. Undo + Redo
 2. Restore Original
 3. Save Changes
+4. Grayscale - Saving image
--- main.py
+++ main.py
@@ -46,6 +46,21 @@ def padding(img, width):
 	display_image(img)
 	return img
 
+
+# make red, green and blue filters
+def rgb_channel(img):
+	img = np.array(img)
+	img_R, img_G, img_B = img.copy(), img.copy(), img.copy()
+	img_R[:, :, (1, 2)] = 0
+	img_G[:, :, (0, 2)] = 0
+	img_B[:, :, (0, 1)] = 0
+	img_rgb = np.concatenate((img_R,img_G,img_B), axis=1)
+	plt.figure(figsize=(15, 15))
+	plt.imshow(img_rgb)
+	plt.show()
+
+
+
 '''
 1. Display Image
 2. Rotate Left
@@ -55,30 +70,42 @@ def padding(img, width):
 6. Grayscale Image
 7. Pad Image
 8. Visualize RGB Channels (Don't save image) 
+9. Colour Reduction
+10. Trim Image
+11. Paste image with slice
+12. Binarize Image
+13. Flip Image
+14. Blend Two Images
+15. Mask Image
+16. Plot Histogram for Pixel Density
+0. Quit
 '''
 
-choice = 7
+choice = 1
 
-# while choice != -1: 
-# 	choice = int(input("Enter your choice: "))
-	
-if choice == 1:
-	display_image(img)
-elif choice == 2:
-	img = rotate_left(img)
-elif choice == 3:
-	img = rotate_right(img)
-elif choice == 4:
-	direction = input("Enter direction to rotate (r or l): ")
-	angle = int(input("Enter angle to rotate by: "))
-	if direction == 'l':
-		angle = 360 - angle
-	img = rotate_by_angle(img, angle)
-elif choice == 5:
-	img = negative(img)
-elif choice == 6:
-	img = grayscale(img)
-elif choice == 7:
-	width = int(input("Enter padding width: "))
-	img = padding(img, width)
-	
+while choice != 0: 
+	choice = int(input("Enter your choice: "))
+	if choice == 1:
+		display_image(img)
+	elif choice == 2:
+		img = rotate_left(img)
+	elif choice == 3:
+		img = rotate_right(img)
+	elif choice == 4:
+		direction = input("Enter direction to rotate (r or l): ")
+		angle = int(input("Enter angle to rotate by: "))
+		if direction == 'l':
+			angle = 360 - angle
+		img = rotate_by_angle(img, angle)
+	elif choice == 5:
+		img = negative(img)
+	elif choice == 6:
+		img = grayscale(img)
+	elif choice == 7:
+		width = int(input("Enter padding width: "))
+		img = padding(img, width)
+	elif choice == 8:
+		rgb_channel(img)
+		
+		
+
